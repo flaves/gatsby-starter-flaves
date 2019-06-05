a0d36164462357aa6384ce6d31f19551790ffc62
Sebastien Crepin <seb_crep@hotmail.com>
2019-06-05 20:15:42 +0200

    Rename pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,11 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const NotFoundPage = () => (
+const Error = () => (
   <Layout>
     <SEO title="404: Not found" />
     <h1>Page not found</h1>
   </Layout>
 );
 
-export default NotFoundPage;
+export default Error;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,11 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+const Home = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hello</h1>
   </Layout>
 );
 
-export default IndexPage;
+export default Home;
