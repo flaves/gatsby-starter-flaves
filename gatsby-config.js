c51d332378a305c13937b620f3dc234712a622c7
Sebastien Crepin <seb_crep@hotmail.com>
2019-06-05 19:40:40 +0200

    Add SASS in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,11 +24,10 @@ module.exports = {
         background_color: `#0F111A`,
         theme_color: `#0F111A`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-sass`
   ],
 }
