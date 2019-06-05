664a780f0fc391e16dd35427423f1eaffd298b59
Sebastien Crepin <seb_crep@hotmail.com>
2019-06-05 20:07:29 +0200

    Clean layout + seo component

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,14 +1,10 @@
-import React from "react"
-import * as PropTypes from "prop-types"
+import React from 'react';
+import * as PropTypes from 'prop-types';
 
-const Layout = ({ children }) => (
-  <main role="main">
-    {children}
-  </main>
-)
+const Layout = ({ children }) => <main role="main">{children}</main>;
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
