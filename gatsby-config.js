91c59564061efa691e97e6cb6bf9659f9e00d9c1
Sebastien Crepin <seb_crep@hotmail.com>
2019-06-05 20:05:58 +0200

    Clean index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,6 +55,14 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /svg/,
+        },
+      },
+    },
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-netlify`,
   ],
