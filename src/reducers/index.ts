5f9c763d74684aa616e42c546d30d088fe1733fe
Sebastien Crepin <seb_crep@hotmail.com>
2019-12-25 17:25:03 +0100

    Update dependencies + fix types

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
 
-import bookingReducer from './bookingReducer';
 import eventReducer from './eventReducer';
-import storeReducer from './storeReducer';
 
 export const rootReducer = combineReducers({
-  booking: bookingReducer,
   event: eventReducer,
-  store: storeReducer,
 });
 
 export type State = ReturnType<typeof rootReducer>;
