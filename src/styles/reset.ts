5abd8496ec15324ab0c96c9477d02b1b347fc7b2
Sebastien <sebastien@flav.es>
2020-08-24 17:27:11 +0200

    feat: improve reset

--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -1,13 +1,39 @@
 import { css } from '@emotion/core';
 
+import { BiennaleFont } from './fonts';
+import marks from './marks';
+
 const reset = css`
-  * {
-    font-family: Helvetica Neue, sans-serif;
+  ${process.env.NODE_ENV === `development` && marks}
+
+  ${BiennaleFont};
+
+  *,
+  *:after,
+  *:before {
+    box-sizing: inherit;
+  }
+
+  html {
+    font-family: Biennale, Helvetica Neue, sans-serif;
+    height: 100%;
   }
 
   body {
+    box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  button {
+    font-family: Biennale, Helvetica Neue, sans-serif;
   }
 
   h1,
